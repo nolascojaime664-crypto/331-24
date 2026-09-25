43509b26f169649461dd006436e7edfd911b9171
nolascojaime664-crypto <nolascojaime664@gmail.com>
2026-09-24 21:31:06 -0700

    Add files via upload

---
+++ CSharp/Cs1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// Espacio de nombres
+namespace Cs1
+{
+    
+    // Clase principal
+
+    class Program
+    {
+        // Funcion principal
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programación en C#");
+        } // Termino de la funcion principal
+    } // Termino de la clase principal
+} // Termino del espacio de nombres
